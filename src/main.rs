94b4eec84184a0f3112cdc59f4d7f6626049e1ef
Sergey Kalistratov <rimusolamus@gmail.com>
2022-12-06 00:55:55 +0100

    day 4

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,6 @@ fn day_four() -> io::Result<()> {
 
     let mut lines_with_dup = 0;
 
-
     for line in reader.lines() {
         let line = line?;
         let divided_line = line.split(",").collect::<Vec<&str>>();
@@ -25,7 +24,9 @@ fn day_four() -> io::Result<()> {
         let full_left = (divided_left[0].parse::<i32>().unwrap()..divided_left[1].parse::<i32>().unwrap() + 1).collect::<Vec<i32>>();
         let divided_right = divided_line[1].split("-").collect::<Vec<&str>>();
         let full_right = (divided_right[0].parse::<i32>().unwrap()..divided_right[1].parse::<i32>().unwrap() + 1).collect::<Vec<i32>>();
-        if full_left.iter().all(|item| full_right.contains(item)) || full_right.iter().all(|item| full_left.contains(item)) {
+        // probably a better way to do this is to just make a vector of all the numbers and then check if there are any duplicates
+        // but I don't care enough to do that
+        if full_left.iter().any(|&x| full_right.contains(&x)) || full_right.iter().any(|&x| full_left.contains(&x)) {
             lines_with_dup += 1;
         }
     }
