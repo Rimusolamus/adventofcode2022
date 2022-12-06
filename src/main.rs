dc011f042a2d7bdd811287e3def5f514a37ba923
Sergey Kalistratov <rimusolamus@gmail.com>
2022-12-06 03:23:34 +0100

    day 5

--- src/main.rs
+++ src/main.rs
@@ -12,60 +12,6 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
-fn day_five() -> io::Result<()> {
-    /*      [M]     [B]             [N]
-    [T]     [H]     [V] [Q]         [H]
-    [Q]     [N]     [H] [W] [T]     [Q]
-    [V]     [P] [F] [Q] [P] [C]     [R]
-    [C]     [D] [T] [N] [N] [L] [S] [J]
-    [D] [V] [W] [R] [M] [G] [R] [N] [D]
-    [S] [F] [Q] [Q] [F] [F] [F] [Z] [S]
-    [N] [M] [F] [D] [R] [C] [W] [T] [M]
-     1   2   3   4   5   6   7   8   9
-
-     1 param - amount
-     2 param - from
-     3 param - to */
-
-    let file = File::open("day5.input")?;
-    let reader = BufReader::new(file);
-
-    let mut base: Vec<Vec<&str>> = Vec::new();
-    base.push(Vec::from(["N", "S", "D", "C", "V", "Q", "T"]));
-    base.push(Vec::from(["M", "F", "V"]));
-    base.push(Vec::from(["F", "Q", "W", "D", "P", "N", "H", "M"]));
-    base.push(Vec::from(["D", "Q", "R", "T", "F"]));
-    base.push(Vec::from(["R", "F", "M", "N", "Q", "H", "V", "B"]));
-    base.push(Vec::from(["C", "F", "G", "N", "P", "W", "Q"]));
-    base.push(Vec::from(["W", "F", "R", "L", "C", "T"]));
-    base.push(Vec::from(["T", "Z", "N", "S"]));
-    base.push(Vec::from(["M", "S", "D", "J", "R", "Q", "H", "N"]));
-    // base.push(Vec::from(["Z", "N"]));
-    // base.push(Vec::from(["M", "C", "D"]));
-    // base.push(Vec::from(["P"]));
-    /* test data
-1,2,1
-3,1,3
-2,2,1
-1,1,2
-     */
-
-    for line in reader.lines() {
-        let line = line?;
-        let split_line = line.split(",").collect::<Vec<&str>>();
-        for _ in 0..split_line[0].parse().unwrap() {
-            let from_crate = base[(split_line[1].parse::<i32>().unwrap() - 1) as usize].pop();
-            base[(split_line[2].parse::<i32>().unwrap() - 1) as usize].push(from_crate.unwrap());
-        }
-    }
-
-    for i in 0..base.len() {
-        print!("{}", base[i].last().unwrap());
-    }
-
-    Ok(())
-}
-
 fn day_one() -> io::Result<()> {
     let file = File::open("input.txt")?;
     let reader = BufReader::new(file);
@@ -211,6 +157,54 @@ fn day_four() -> io::Result<()> {
     }
 
     println!("{}", lines_with_dup);
+
+    Ok(())
+}
+
+fn day_five() -> io::Result<()> {
+    /*      [M]     [B]             [N]
+    [T]     [H]     [V] [Q]         [H]
+    [Q]     [N]     [H] [W] [T]     [Q]
+    [V]     [P] [F] [Q] [P] [C]     [R]
+    [C]     [D] [T] [N] [N] [L] [S] [J]
+    [D] [V] [W] [R] [M] [G] [R] [N] [D]
+    [S] [F] [Q] [Q] [F] [F] [F] [Z] [S]
+    [N] [M] [F] [D] [R] [C] [W] [T] [M]
+     1   2   3   4   5   6   7   8   9
+
+     1 param - amount
+     2 param - from
+     3 param - to */
+
+    let file = File::open("day5.input")?;
+    let reader = BufReader::new(file);
+
+    let mut base: Vec<Vec<&str>> = Vec::new();
+    base.push(Vec::from(["N", "S", "D", "C", "V", "Q", "T"]));
+    base.push(Vec::from(["M", "F", "V"]));
+    base.push(Vec::from(["F", "Q", "W", "D", "P", "N", "H", "M"]));
+    base.push(Vec::from(["D", "Q", "R", "T", "F"]));
+    base.push(Vec::from(["R", "F", "M", "N", "Q", "H", "V", "B"]));
+    base.push(Vec::from(["C", "F", "G", "N", "P", "W", "Q"]));
+    base.push(Vec::from(["W", "F", "R", "L", "C", "T"]));
+    base.push(Vec::from(["T", "Z", "N", "S"]));
+    base.push(Vec::from(["M", "S", "D", "J", "R", "Q", "H", "N"]));
+
+    for line in reader.lines() {
+        let line = line?;
+        let split_line = line.split(",").collect::<Vec<&str>>();
+        let from_crate = base[(split_line[1].parse::<i32>().unwrap() - 1) as usize]
+            .as_slice()[base[(split_line[1].parse::<i32>().unwrap() - 1) as usize].len() - (split_line[0].parse::<i32>().unwrap() as usize)..]
+            .to_vec();
+        let final_len = base[(split_line[1].parse::<i32>().unwrap() - 1) as usize].len().saturating_sub(split_line[0].parse::<i32>().unwrap() as usize);
+        base[(split_line[1].parse::<i32>().unwrap() - 1) as usize].truncate(final_len);
+        base[(split_line[2].parse::<i32>().unwrap() - 1) as usize].append(&mut from_crate.clone());
+
+    }
+
+    for i in 0..base.len() {
+        print!("{}", base[i].last().unwrap());
+    }
 
     Ok(())
 }
