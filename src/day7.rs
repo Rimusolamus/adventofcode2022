fb9c5b99a1a3f649db720ff00f964bb95103f8da
Sergey Kalistratov <rimusolamus@gmail.com>
2022-12-08 03:45:23 +0100

    save state

---
+++ src/day7.rs
@@ -0,0 +1,7 @@
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
+
+pub(crate) fn day_seven() -> io::Result<()> {
+    println!("Have a nice day!");
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,17 @@ mod day4;
 mod day3;
 mod day2;
 mod day1;
+mod day7;
 
 use std::io::{self};
 
 fn main() -> io::Result<()> {
-    // day1::day_one().expect("day1 failed");
-    // day2::day_two().expect("day2 failed");
-    // day3::day_three().expect("day3 failed");
-    // day4::day_four().expect("day4 failed");
-    // day5::day_five().expect("day5 failed");
-    // day6::day_six().expect("day6 failed");
+    day1::day_one().expect("day1 failed");
+    day2::day_two().expect("day2 failed");
+    day3::day_three().expect("day3 failed");
+    day4::day_four().expect("day4 failed");
+    day5::day_five().expect("day5 failed");
+    day6::day_six().expect("day6 failed");
     println!("Have a nice day!");
     Ok(())
 }
